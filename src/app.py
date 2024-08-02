813d5140c326e9948acc518e89d162c0fc594634
SamuelHollis <samuelhollis16@gmail.com>
2024-08-02 13:39:33 +0200

    modelo_ok

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask
+app = Flask(__name__)
 
-# your code here
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+export FLASK_APP=app
