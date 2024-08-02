3158a22cdc4886ddda360563ba36813cb47917f0
SamuelHollis <samuelhollis16@gmail.com>
2024-08-02 15:26:11 +0200

    fin

--- src/app.py
+++ src/app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from pickle import load
+
 app = Flask(__name__)
+model = load(open(r"C:\Users\samue\OneDrive\Escritorio\Docs\4GeeksAcademy\29a clase-Despliegue_modelos_AI_en_Render.com_usando_Flask\Flask_project\src\decision_tree_regressor_default_42.sav", "rb"))
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+def home():
+    return render_template('index.html')
 
-export FLASK_APP=app
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Obtener los datos del formulario
+    input_data = [float(request.form['Time']),
+                  float(request.form['Amount'])] + [float(request.form[f'V{i}']) for i in range(1, 29)]
+    
+    # Realizar la predicción
+    prediction = model.predict([input_data])
+    
+    # Determinar el resultado
+    result = 'Fraudulent' if prediction[0] == 1 else 'Not Fraudulent'
+    
+    return render_template('index.html', prediction_text=f'Transaction is {result}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
